148317b4c6165c0ce993dae9af84511f44bc236b
ktotakoivasya <113380796+ktotakoivasya@users.noreply.github.com>
2024-02-19 20:47:35 +0300

    старт

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import opennlp.tools.doccat.DoccatModel;
+
+import java.io.FileInputStream;
+
+public class Main {
+    String questionsText = "nlp-edu/questions.txt";
+    String questionsBin = "nlp-models/questions.bin";
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void trainDoccatModel() {
+
+    }
+}
